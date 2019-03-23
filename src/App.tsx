37ab91665a1cd2c8eddde42208805c06b15d5ad2
LAPTOP-HNHS6VB9\Paul <paul@novonetworks.com>
2019-03-23 14:03:51 +0900

    기타: TS 개발환경 설정

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+
+class App extends Component {
+  public render() {
+    const ab: string = 'test';
+    return (
+      <div className="App">
+        <Button>hello</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'react-app-polyfill/ie11';
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
